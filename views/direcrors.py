6b08d175149e1b0cfd1a961d083b1b05c0d41fa3
RSaveiko <95852663+RSaveiko@users.noreply.github.com>
2022-03-19 14:29:22 +0600

    Create direcrors.py

---
+++ views/direcrors.py
@@ -0,0 +1,31 @@
+
+from flask_restx import Resource, Namespace
+from models import Director, DirectorSchema
+
+directors_ns = Namespace('directors')
+director_schema = DirectorSchema()
+directors_schema = DirectorSchema(many=True)
+from setup_db import db
+
+
+
+@directors_ns.route("/")
+class DirectorsView(Resource):
+    def get(self):
+        directors = db.session.query(Director).all()
+        if directors:
+            return directors_schema.dump(directors), 200
+        else:
+            return "", 404
+
+
+
+@directors_ns.route('/<int:nid>')
+class DirectorView(Resource):
+    def get(self, nid):
+        genre = db.session.query(Director).get(nid)
+        if genre:
+            return director_schema.dump(genre), 200
+        else:
+            return "", 404
+
