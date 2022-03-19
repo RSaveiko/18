7098f93173b53c7867b6f76015c6ce6eff2ddf79
RSaveiko <95852663+RSaveiko@users.noreply.github.com>
2022-03-19 14:24:29 +0600

    Create models.py

---
+++ models.py
@@ -0,0 +1,48 @@
+from marshmallow import Schema, fields
+from setup_db import db
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    trailer = db.Column(db.String)
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Integer)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
+    genre = db.relationship("Genre")
+    director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
+    director = db.relationship("Director")
+
+
+class Director(db.Model):
+    __tablename__ = 'director'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+
+class Genre(db.Model):
+    __tablename__ = 'genre'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+
+
+
+class MovieSchema(Schema):
+    id = fields.Int(dump_only=True)
+    text = fields.Str()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+
+
+class GenreSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+
+
+class DirectorSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
