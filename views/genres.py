b8714480462a85b9be073b373f86a25fe3c075f0
RSaveiko <95852663+RSaveiko@users.noreply.github.com>
2022-03-19 14:29:48 +0600

    Create genres.py

---
+++ views/genres.py
@@ -0,0 +1,30 @@
+
+from flask_restx import Resource, Namespace
+from models import Genre, GenreSchema
+
+genres_ns = Namespace('genres')
+genre_schema = GenreSchema()
+genres_schema = GenreSchema(many=True)
+
+from setup_db import db
+
+@genres_ns.route("/")
+class GenresView(Resource):
+    def get(self):
+        genres = db.session.query(Genre).query.all()
+        if genres:
+            return genres_schema.dump(genres), 200
+        else:
+            return "", 404
+
+
+
+@genres_ns.route('/<int:nid>')
+class GenreView(Resource):
+    def get(self, nid):
+        genre = db.session.query(Genre).query.get(nid)
+        if genre:
+            return genre_schema.dump(genre), 200
+        else:
+            return "", 404
+
